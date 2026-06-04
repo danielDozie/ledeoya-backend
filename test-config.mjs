9039d3401f96b0bc1f4a6de9cff70848027d7884
danielDozie <placmedia@gmail.com>
2026-06-04 14:28:51 +0100

    refactor: remove unused test files and update .gitignore

--- test-config.mjs
+++
@@ -1,4 +0,0 @@
-import { starterCollections, mediaCollections, authCollections, coreSettingsGlobals } from '@kyro-cms/core/templates';
-const collections = [...Object.values(starterCollections), ...Object.values(mediaCollections), ...Object.values(authCollections)];
-console.log('collections:', collections.length);
-console.log('globals:', coreSettingsGlobals.length);
